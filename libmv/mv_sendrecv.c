4cab24e9568a25dd45236a554891594ed539e025
CJ <mv3142@gmail.com>
2015-02-01 21:57:18 -0800

    socket-based messagign set to default

--- libmv/mv_sendrecv.c
+++ libmv/mv_sendrecv.c
@@ -5,15 +5,11 @@
  */
 
 /* 
-#define USE_SOCKTCP
-#define USE_ZEROMQ
+   #define USE_ZEROMQ
 */
-#define USE_ZEROMQ
 
-#if defined(USE_SOCKTCP)
-#  include "mv_sendrecv_sock.c"
-#elif defined(USE_ZEROMQ)
+#if defined(USE_ZEROMQ)
 #  include "mv_sendrecv_zmq.c"
 #else
-#  error Only ZeroMQ-based implementation is available.
+#  include "mv_sendrecv_sock.c"
 #endif
